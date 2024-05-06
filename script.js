d8d999837195c79fbc21634844f921e053cf9eaa
leonyama <leonyama.728@gmail.com>
2024-05-07 01:58:19 +0900

    new commit

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    document.querySelectorAll('.line').forEach(function(line) {
-      line.classList.toggle('active');
-    });
-  
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('open');
-  });
-/*三本線あるだけです。開きません。助けてください*/  

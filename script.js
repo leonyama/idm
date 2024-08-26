97d8676b5b7c7016b9340b68f7ea432a9067295a
leonyama <leonyama.728@gmail.com>
2024-08-27 02:51:53 +0900

    add blog

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDetails(id) {
+    var details = document.getElementById(id);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}

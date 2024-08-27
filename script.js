9919b6642e3777035bd9618fb884fbf395041cd8
leonyama <leonyama.728@gmail.com>
2024-08-27 19:28:51 +0900

    new commit

--- script.js
+++
@@ -1,8 +0,0 @@
-function showDetails(id) {
-    var details = document.getElementById(id);
-    if (details.style.display === "none" || details.style.display === "") {
-        details.style.display = "block";
-    } else {
-        details.style.display = "none";
-    }
-}
